64d1bfefbe9be2e8ff5edb952f0804044bf1012b
LorenzoPorrega <lorenzoporrega@hotmail.it>
2023-07-22 15:38:27 +0200

    added function to print the capitalized initials

--- js/main.js
+++ js/main.js
@@ -62,43 +62,62 @@ const postId = [];
 
 posts.forEach((singlePost, i) => {
   container.innerHTML += `<div class="post">
-                          <div class="post__header">
-                              <div class="post-meta">                    
-                                  <div class="post-meta__icon">
-                                      <img class="profile-pic" src="${singlePost.author.image}" alt="${singlePost.author.name}">                    
-                                  </div>
-                                  <div class="post-meta__data">
-                                      <div class="post-meta__author">${singlePost.author.name}</div>
-                                      <div class="post-meta__time">4 mesi fa</div>
-                                  </div>                    
-                              </div>
-                          </div>
-                          <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
-                          <div class="post__image">
-                              <img src="${singlePost.media}" alt="${singlePost.author.name}">
-                          </div>
-                          <div class="post__footer">
-                              <div class="likes js-likes">
-                                  <div class="likes__cta">
-                                      <a class="like-button  js-like-button" data-postid="${i}">
-                                          <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                                          <span class="like-button__label">Mi Piace</span>
-                                      </a>
-                                  </div>
-                                  <div class="likes__counter">
-                                      Piace a <b id="like-counter-1" class="js-likes-counter">${singlePost.likes}</b> persone
-                                  </div>
-                              </div> 
-                          </div>            
-                        </div>`
+                            <div class="post__header">
+                                <div class="post-meta">                    
+                                    <div class="post-meta__icon">
+                                        <img class="profile-pic" src="${singlePost.author.image}" alt="${singlePost.author.name}">                    
+                                    </div>
+                                    <div class="post-meta__data">
+                                        <div class="post-meta__author">${singlePost.author.name}</div>
+                                        <div class="post-meta__time">4 mesi fa</div>
+                                    </div>                    
+                                </div>
+                            </div>
+                            <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+                            <div class="post__image">
+                                <img src="${singlePost.media}" alt="${singlePost.author.name}">
+                            </div>
+                            <div class="post__footer">
+                                <div class="likes js-likes">
+                                    <div class="likes__cta">
+                                        <a class="like-button  js-like-button" data-postid="${i}">
+                                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                            <span class="like-button__label">Mi Piace</span>
+                                        </a>
+                                    </div>
+                                    <div class="likes__counter">
+                                        Piace a <b id="like-counter-1" class="js-likes-counter">${singlePost.likes}</b> persone
+                                    </div>
+                                </div> 
+                            </div>            
+                          </div>`;
+  if(singlePost.author.image === null){
+    const profilePic = document.querySelectorAll(".post-meta__icon");
+    profilePic[i].classList.add("profile-pic-default");
+    const iconDiv = document.querySelector(".profile-pic-default");
+    const capitalInitialsFunct =  capitalInitials(singlePost);
+    iconDiv.innerHTML = `<span>${capitalInitialsFunct}</span>`;
+  }
 postId.push(i);                     
 });
 
-console.log(postId)
-const likesButton = document.querySelector(`[data-postid="${postId[0]}"]`);
+function capitalInitials(x){
+  let authorName = x.author.name;
+  const splitName = authorName.split(" ");
+  const firstName = splitName[0][0];
+  const lastName = splitName[1][0];
+  const initials = firstName + lastName;
+  return initials;
+}
 
-likesButton.addEventListener("click", function(){
-  const likesIcon = document.querySelector(".like-button");
-  likesIcon.classList.toggle("like-button--liked");
-  console.log("Cliccato")
-});
+console.log(postId);
+
+const likeButtons = document.querySelectorAll('.like-button');
+const likeCounters = document.querySelectorAll('like-counter-1');
+
+for (let i = 0; i < likeButtons.length; i++) {
+  const element = likeButtons[i];
+  element.addEventListener('click', function (e){
+    likeButtons[i].classList.toggle("liked-button");
+  });
+}
